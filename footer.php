266dfb8a9cce2dce1087333cd03dcd55b886512a
bmcculley <zen.fed96@gmail.com>
2014-03-31 01:59:20 -0400

    dot com update

--- footer.php
+++ footer.php
@@ -9,7 +9,7 @@
 	<div class="clearfix">&nbsp;</div>
 	<footer id="colophon" role="contentinfo">
 		<div id="theme-credit" class="alignright">
-			<a href="<?php echo esc_url('http://tempvault.github.io'); ?>">Template Vault</a> Default theme for WordPress by <a href="<?php echo esc_url('http://bmcculley.github.io'); ?>" title="B.McCulley">B. McCulley</a>
+			<a href="<?php echo esc_url('http://tempvault.com'); ?>">Template Vault</a> Default theme for WordPress by <a href="<?php echo esc_url('http://bmcculley.github.io'); ?>" title="B.McCulley">B. McCulley</a>
 		</div><!-- #theme-credit -->
 		<div class="clearfix">&nbsp;</div>
 	</footer><!-- #colophon -->
